a585abe3edde488c21122a652b9d1bb3082892c9
Patella <jfpatella@gmail.com>
2024-08-12 17:11:25 -0300

    Criação da função gerarResultado(). Estruturação dos inputs e button no html.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log("Script funcionando");
 
-function soma (numero1, numero2){
-var soma = numero1 + numero2;
-return soma;
+function soma(numero1, numero2){
+var resultado = numero1 + numero2;
+return resultado;
+}
+
+function gerarResultado(){
+    return alert ("resultado");
 }
